8a906f94f504e7856e2df81ab372b57838721bb7
vinayaksh42 <vinayaksh42@gmail.com>
2021-08-26 10:53:22 +0530

    storybook fix

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,33 +1,34 @@
-
-const path = require('path')
+const path = require("path");
 
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app"
+    {
+      name: "@storybook/addon-postcss",
+      options: {
+        postcssLoaderOptions: {
+          implementation: require("postcss"),
+        },
+      },
+    },
   ],
-  webpackFinal: async (config) => {
+  webpackFinal: async (config, { configType }) => {
     config.module.rules.push({
-      test: /\,css&/,
+      test: /\.scss$/,
       use: [
-        {
-          loader: 'postcss-loader',
-          options: {
-            ident: 'postcss',
-            plugins: [
-              require('tailwindcss'),
-              require('autoprefixer')
-            ]
-          }
-        }
+        require.resolve("tailwindcss"),
+        require.resolve("autoprefixer"),
       ],
-      include: path.resolve(__dirname, '../'),
-    })
-    return config
-  }
-}
+      include: path.resolve(__dirname, "../"),
+    });
+
+    config.module.rules.push({
+      test: /\.pug$/,
+      use: [{ loader: "pug-plain-loader" }],
+    });
+
+    return config;
+  },
+};
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,12 @@
 import '../src/index.css';
+import '../src/index.css';
+import '../src/components/atoms/button/button.css';
+import '../src/components/atoms/category-button/category-button.css';
+import '../src/components/atoms/exit/exit.css';
+import '../src/components/atoms/upload/upload.css';
+import '../src/components/molecules/workflow/workflow.css';
+import '../src/components/organisms/organisms.css';
+import '../src/pages/template/template.css';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
